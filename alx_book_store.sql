81c25e62e752aa557fa20fc8c593db5f034ebe37
reuben-idan <nobeadonis2@gmail.com>
2025-06-28 13:02:47 +0000

    Create A Magical Database for a Dream Online Bookstore Adventure!

---
+++ alx_book_store.sql
@@ -0,0 +1,48 @@
+-- A Magical Database for Your Dream Online Bookstore Adventure!
+-- Database Name: alx_book_store
+
+-- Create the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+-- Authors table: Stores information about authors
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Customers table: Stores information about customers
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT NOT NULL
+);
+
+-- Books table: Stores information about books available in the bookstore
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Orders table: Stores information about orders placed by customers
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Order_Details table: Stores information about the books included in each order
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+); 
