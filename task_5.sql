bc47d352d9272fd9679526c8ccb7c69f300f0ca1
reuben-idan <nobeadonis2@gmail.com>
2025-06-28 13:40:16 +0000

    update code snippet

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,7 @@
 -- Populating Your Database with Your Very First Data
--- Insert a single row in the Customers table
+-- Insert a single row in the customer table
 
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address) 
+INSERT INTO customer (customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'); 
